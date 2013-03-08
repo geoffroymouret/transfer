59d087a7e30996b97b89c3fd9951d3b313430d96
Geoffroy MOURET <geoffroy.mouret@polymtl.ca>
2013-03-08 18:14:51 -0500

    input2MLP_LR_Momentum_Preproc

--- start.py
+++ start.py
@@ -3,6 +3,6 @@ from pylearn2.utils.shell import run_shell_command
 
 myDir = "/data/lisatmp/ift6266h13/mouretge/transfer/"
 
-command = """jobdispatch --condor --env=THEANO_FLAGS=floatX=float32 --duree=48:00:00 --whitespace python %(dir)strain_model.py %(dir)syaml_files/1layerSoftmax_LR0.\"{{1,O5,01,005,001}}\".yaml""" % {"dir":myDir}
+command = """jobdispatch --condor --env=THEANO_FLAGS=floatX=float32 --duree=48:00:00 --whitespace python %(dir)strain_model.py %(dir)syaml_files/2MLPSoftmax_LR0.\"{{1,O5,01,005,001}}\"_Momentum0.\"{{1,3,5,7,9}}\"".yaml""" % {"dir":myDir}
 run_shell_command(command)
 print command
