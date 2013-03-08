49a4dcac30828ce276a36e3257b6e0193a9a9774
Geoffroy Mouret <mouretge@bart1.iro.umontreal.ca>
2013-03-08 18:00:39 -0500

    firstSetLR

--- start.py
+++ start.py
@@ -3,5 +3,6 @@ from pylearn2.utils.shell import run_shell_command
 
 myDir = "/data/lisatmp/ift6266h13/mouretge/transfer/"
 
-command = """jobdispatch --condor --env=THEANO_FLAGS=device=gpu,floatX=float32,force_device=True --duree=48:00:00 --whitespace --gpu bash %(dir)strain_model.py \"%(dir)s/yaml_files/1layerSoftmax_LR0.1.yaml\" """ % {"dir":myDir}
+command = """jobdispatch --condor --env=THEANO_FLAGS=floatX=float32 --duree=48:00:00 --whitespace python %(dir)strain_model.py %(dir)syaml_files/1layerSoftmax_LR0.\"{{1,O5,01,005,001}}\".yaml""" % {"dir":myDir}
 run_shell_command(command)
+print command
