530c766586df25816e23c01fb0075eefd83d5c5f
Geoffroy MOURET <geoffroy.mouret@polymtl.ca>
2013-03-08 19:19:27 -0500

    modif_start

--- start.py
+++ start.py
@@ -2,7 +2,16 @@ import os
 from pylearn2.utils.shell import run_shell_command
 
 myDir = "/data/lisatmp/ift6266h13/mouretge/transfer/"
+myYamls = myDir + "yaml_files/"
 
-command = """jobdispatch --condor --env=THEANO_FLAGS=floatX=float32 --duree=48:00:00 --whitespace python %(dir)strain_model.py %(dir)syaml_files/2MLPSoftmax_LR0.\"{{1,O5,01,005,001}}\"Momentum0.\"{{1,3,5,7,9}}\"".yaml""" % {"dir":myDir}
+names = os.listdir(myYamls)
+dirs = [myYamls + name for name in names]
+
+command = """jobdispatch --condor --env=THEANO_FLAGS=floatX=float32 --duree=48:00:00 --whitespace python %(dir)strain_model.py """ % {"dir":myDir}
+
+command += """\"{{"""
+command += ','.join(dirs)
+command += """}}\""""
+
 run_shell_command(command)
 print command
